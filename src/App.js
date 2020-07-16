1180f69aa8880b6ea247f5040228bbb1b578ca58
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-07-16 18:44:26 +0800

    implemented react-router-dom and other routing features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Directory from './components/directory/Directory.component';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Calculator from './components/calculator/Calculator.component';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/' component={ Directory } />
+				<Route exact path='/calculator' component={Calculator} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/directory/Directory.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import Project from '../project/Project.component';
+
+const DirectoryWrapper = styled.div`
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-rows: 1fr 1fr 1fr 1fr;
+	justify-items: center;
+`;
+
+const Directory = () => {
+	return (
+		<DirectoryWrapper>
+			<Project title='calculator'/>
+		</DirectoryWrapper>
+	);
+};
+
+export default Directory;
